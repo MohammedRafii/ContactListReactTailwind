5c52ba8751b39beee4eb94451c9d836a08703a5d
MohammedRafii <121567736+MohammedRafii@users.noreply.github.com>
2023-07-18 09:50:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,7 @@ function App() {
   
   return (
     <>
-    <div className="w-4/5 mx-auto">
-      <h1>Created by Mohammed Rafi T S</h1>
+    <div className="w-4/5 mx-auto md:my-28">
      <ContactsCard contacts={contacts} />
      </div>
     </>
